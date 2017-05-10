16e980b0d2a8f20ed761330f2546f76cd141b12e
Andrey Miskov <amiskov@gmail.com>
2017-05-10 05:43:36 +0300

    Fix comment list

--- src/CommentList.js
+++ src/CommentList.js
@@ -2,6 +2,12 @@ import React, {Component} from 'react'
 import Comment from './Comment'
 
 class CommentList extends Component {
+    static defaultProps = {
+        // если каменты из пропсов не пришли, то пусть будет пустой массив,
+        // чтоб в любом случае было, что итерировать и не делать проверки !comments
+        comments: []
+    }
+
     state = {
         show: false
     };
@@ -13,11 +19,7 @@ class CommentList extends Component {
     };
 
     render() {
-        const {comments} = this.props;
-
-        if (!comments) {
-            return null;
-        }
+        const {comments} = this.props; // если каментов нет, то тут будет пустой массив из defaultProps
 
         const elements = comments.map((comment) => {
             return (
@@ -29,6 +31,10 @@ class CommentList extends Component {
 
         const getText = () => {
             return this.state.show ? 'Hide comments' : 'Show comments'
+        }
+
+        if(!comments.length) {
+            return <p>No comments yet</p>;
         }
 
         return (
