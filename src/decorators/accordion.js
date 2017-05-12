db79f7ef05ff1676e68c5621243ee545352fedd4
Andrey Miskov <amiskov@gmail.com>
2017-05-12 08:54:28 +0300

    Improve checking open section

--- src/decorators/accordion.js
+++ src/decorators/accordion.js
@@ -17,7 +17,8 @@ export default Sections => class Accordion extends Component {
     }
 
     // Инкапсулируем логику в декораторе,
-    // передаем дополнительную функцию для проверки, открыта ли секция
+    // передаем дополнительную функцию для проверки, открыта ли секция. Тут может быть любая логика в будущем,
+    // может быть не просто проверка айдишников на равенство. Все тут, ArticleList никак не участвует:
     isSectionOpen = sectionId => this.state.openSectionId === sectionId; // `this` будет привязан
 
     // Для лучшего понимания, вариант на ES5
@@ -27,7 +28,7 @@ export default Sections => class Accordion extends Component {
 
     toggleSection = openSectionId => ev => {
         this.setState({
-            openSectionId: (this.state.openSectionId === openSectionId) ? null : openSectionId
+            openSectionId: this.isSectionOpen(openSectionId) ? null : openSectionId
         })
     }
 }
